e9aa273929d2e3d41334365a3f80140dcddf51af
Phillip Salazar <areabio52@gmail.com>
2018-05-03 16:18:45 -0500

    lab06 update 2

--- lab06/main.cpp
+++ lab06/main.cpp
@@ -5,9 +5,8 @@
 
 using namespace std;
 
-
-char generateLetterGrade(int);
-void print_out();
+void openFile(string filename);
+char generateLetterGrade(int num);
 
 
 // a data structure for students grade.
@@ -40,19 +39,24 @@ int main() {
   
     // initialize data structure array and has 28 entries
     struct StudentGrades grades[28] = {};
-    //   struct FinalGrades finalgrades[26] = {};
+    int finalgrades[28] = {};
     // get the size of the array
     int classLength = sizeof(grades)/sizeof(*grades);
-
-
-    int number_of_lines = 0;
+    int exam1_avarage;
+    int exam2_avarage;
+    int exam3_avarage;
+    int final_avarage;
+    int labs_avarage;
+    
     string grade_string;
     int grade;
     int i = 0;
-    
+    int number_of_lines = 0;
     // streaming files and 
     ifstream infile;
     ofstream outfile;
+
+  
 	infile.open ("dataset.txt");
     string dummyLine;
 
@@ -77,7 +81,7 @@ int main() {
     // do know what this piece of code does, but it works. the goal was to aissgin grades from the file to the data structure array.
     while (infile >> name >> var1 >> var2 >> var3 >> var4 >> var5 >> var6 >> var7 >> var8 >> var9 >> var10 >> var11 >> var12 >> var13 >> var14)
     {
-        /* do something with name, var1 etc. */
+        // something with name, var1 etc.
         grades[i].id = name;
         grades[i].exam1 = var1;
         grades[i].exam2 = var2;
@@ -99,15 +103,16 @@ int main() {
         cout << grades[i].id << "\t" << grades[i].exam1 << "\t" << grades[i].exam2 << "\t" << grades[i].exam3 << "\t" << grades[i].quiz1 << "\t" << 
         grades[i].quiz2 << "\t" << grades[i].quiz3 << "\t" << grades[i].lab1 << "\t" << grades[i].lab2 << "\t" << grades[i].lab3 << "\t" << 
         grades[i].lab4 << "\t" << grades[i].lab5 << "\t" << grades[i].lab6 << "\t" << grades[i].journals << "\t" << grades[i].finals  <<"\n";
-        */
-
+        
+*/
        // increase by one, and counts the the number lines that is in the files
         i++;
-        number_of_lines = i;
+        //number_of_lines = i;
     }
        
  
 	infile.close();
+
 
     for (int i = 0; i < classLength; i++){
         grades[i].total_grade = (grades[i].exam1 + grades[i].exam2 + grades[i].exam3 + grades[i].quiz1 + 
@@ -116,11 +121,12 @@ int main() {
 
        grades[i].letter_grade = generateLetterGrade(grades[i].total_grade);
     
+        finalgrades[i] = grades[i].total_grade; 
 
-      // Console out test to see if it works.
-      //    grades[i].letter_grade = ;
-      //   cout << finalgrades[i].id << "\t" << finalgrades[i].finalgrade << "\t" << finalgrades[i].letter_grade << endl;  
-      //  cout << grades[i].letter_grade << endl;
+        exam1_avarage += grades[i].exam1;
+        exam2_avarage += grades[i].exam2;
+        exam3_avarage += grades[i].exam3;
+        final_avarage += grades[i].finals;               
     }
     // Console out test to see if it works.
     //cout << grades[25].id << "\t" << grades[25].exam1 << endl;  
@@ -135,47 +141,35 @@ int main() {
   outfile.close();
 
 // useless search agolrithm 
- long int idHieght;
- int a;
-    for (int i = 0; i << classLength; i++){
-        if ( grades[i].total_grade >= 80 ){
-            a = 1;
+    int a;
+    int b;
+    for (int i = 0; i < 28; i++){
+        if ( finalgrades[i] >= 80 ){
+            a = i;
             break;
         }
     }
 
+    for (int i = 0; i < 28; i++){
+        if (finalgrades[i] < 65 ){
+            b = i;
+            break;
+        }
+    }
     // prints of class size
-    cout << "Class Size " << number_of_lines << endl;
+    cout << "Class Size " << classLength << endl;
     
-    // prints out hieghest score and lowest score. 
-    // had to hard code the highest and lowest grade.
-    cout << "heighest score " << "\tID: " << grades[15].id << "\tScore: " << grades[15].total_grade << "\tLetter grade: " << grades[15].letter_grade << endl;
-    cout << "Lowest score " << "\tID: " << grades[24].id << "\tScore: " << grades[24].total_grade << "\tLetter grade: " << grades[24].letter_grade << endl;
-    
- 
+    //prints out hieghest score and lowest score. 
+    //had to hard code the highest and lowest grade.
+    cout << "highest score " << "\tID: " << grades[a].id << "\tScore: " << grades[a].total_grade << "\tLetter grade: " << grades[a].letter_grade << endl;
+    cout << "Lowest score " << "\tID: " << grades[b].id << "\tScore: " << grades[b].total_grade << "\tLetter grade: " << grades[b].letter_grade << endl;
+    cout << "Exam 1 avarage: " << exam1_avarage/28 << endl;
+    cout << "Exam 2 avarage: " << exam2_avarage/28 << endl;
+    cout << "Exam 3 avarage: " << exam3_avarage/28 << endl;
+    cout << "Final avarage: " << final_avarage/28 << endl;
     return 0;
 } // end of main function
 
-// This function generates letter grades for students
-char generateLetterGrade(int num){
-
-    char lettergrade;
-
-    // a block of logic to test each final grade for the letter grades.
-    if ((num < 70) and (num > 59)){
-        lettergrade = 'D';
-    } else if ((num < 80) and (num > 69)){
-        lettergrade = 'C';
-    } else if ((num < 90) and (num > 79)){
-        lettergrade = 'B';
-    } else if ((num < 101) and (num > 89)){
-        lettergrade = 'A';
-    }else {
-        lettergrade = 'F';
-    }
-    
-    return lettergrade;
-}   // end of function
 
 // a useless bubble sort function
 void BubbleSort (int arr[], int n)
@@ -196,3 +190,27 @@ void BubbleSort (int arr[], int n)
 		// Value at n-i-1 will be maximum of all the values below this index.
 	}	
 }	// end of function
+
+
+// This function generates letter grades for students
+char generateLetterGrade(int num){
+
+
+    char lettergrade;
+
+    // a block of logic to test each final grade for the letter grades.
+    if ((num < 70) and (num > 59)){
+        lettergrade = 'D';
+    } else if ((num < 80) and (num > 69)){
+        lettergrade = 'C';
+    } else if ((num < 90) and (num > 79)){
+        lettergrade = 'B';
+    } else if ((num < 101) and (num > 89)){
+        lettergrade = 'A';
+    }else {
+        lettergrade = 'F';
+    }
+    
+    return lettergrade;
+}   // end of function
+
