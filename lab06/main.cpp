e8701d2e23a85076bd5918a208bd2bb16a042ccc
Phillip Salazar <areabio52@gmail.com>
2018-05-03 16:43:43 -0500

    final product update 2

--- lab06/main.cpp
+++ lab06/main.cpp
@@ -179,28 +179,6 @@ int main() {
     return 0;
 } // end of main function
 
-
-// a useless bubble sort function
-void BubbleSort (int arr[], int n)
-{
-	int i, j;
-	for (i = 0; i < n; ++i)
-	{
-		for (j = 0; j < n-i-1; ++j)
-		{
-			// Comparing consecutive data and switching values if value at j > j+1.
-			if (arr[j] > arr[j+1])
-			{
-				arr[j] = arr[j]+arr[j+1];
-				arr[j+1] = arr[j]-arr[j + 1];
-				arr[j] = arr[j]-arr[j + 1];
-			}
-		}
-		// Value at n-i-1 will be maximum of all the values below this index.
-	}	
-}	// end of function
-
-
 // This function generates letter grades for students
 char generateLetterGrade(int num){
 
